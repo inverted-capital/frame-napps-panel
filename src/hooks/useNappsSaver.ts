301ed1e8c3a9a21d4c8ab262b37aa2f46033ce84
inverted-capital <69952973+inverted-capital@users.noreply.github.com>
2025-06-18 17:55:32 +1200

    fix: handle missing artifact in napps saver

--- src/hooks/useNappsSaver.ts
+++ src/hooks/useNappsSaver.ts
@@ -5,6 +5,7 @@ const useNappsSaver = () => {
   const artifact = useArtifact()
 
   return async (data: NappsData): Promise<void> => {
+    if (!artifact) return
     artifact.files.write.json('napps.json', data)
     await artifact.branch.write.commit('Update napps data')
   }
